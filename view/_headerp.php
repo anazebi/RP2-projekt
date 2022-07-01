7a690584d1e53e250eb1c1cb55f6a38b63085f7c
mpesut <matea.pesut6@gmail.com>
2022-07-01 23:36:13 +0200

    jos nesto u view

---
+++ view/_headerp.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Špeceraj</title>
+</head>
+<body>
+    <h1 class="pocetna"> Dobrodošli u naš Špeceraj! </h1>
+   <br>
