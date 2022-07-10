fa4acc02ab3bd9ece74e4b99220c27a8c1598f1b
anazebi <anazebi.math@pmf.hr>
2022-07-11 01:15:59 +0200

    pretrazivanje po imenu proizvoda & sortiranje po cijeni

--- model/service.class.php
+++ model/service.class.php
@@ -8,6 +8,7 @@ require_once __DIR__ .'/user.class.php';
 
 class Service{
 
+    // funkcija za dohvat proizvoda po identifikatoru, koristimo je u iducim funkcijama za dohvat podataka da smanjimo nepotrebna ponavljanja u kodu
     public static function getProductById($id_product)
     {
         $db = DB::getConnection();
@@ -26,6 +27,29 @@ class Service{
         return $proizvod;
     }
 
+    //funkcija koja korisniku omogucava pretrazivanje proizvoda po imenu
+    public static function getProductByName($product_name)
+    {
+      $db = DB::getConnection();
+      $st = $db->prepare('SELECT * FROM proizvodi');
+      $st->execute([]);
+      $ime_trazilica = strtolower($product_name);
+
+      $products = [];
+      while($row = $st->fetch())
+      {
+        //korisniku dopustamo da unese tek neku od rijeci iz imena proizvoda te mu vracamo sve proizvode koji sadrze danu rijec u imenu
+        //npr za unos kruh u trazilicu korisnik dobiva na raspolaganje i crni i bijeli kruh
+        $ime_baza = strtolower($row['ime']);
+        if(strpos($ime_baza, $ime_trazilica))
+        {
+          $products[] = DB::getProductById($row['id']);
+        }
+      }
+      return $products;
+    }
+
+    //dohvacanje svih proizvoda na rasprodaji
     public static function getProductsOnSale()
     {
         $db = DB::getConnection();
@@ -40,5 +64,69 @@ class Service{
         }
         return $products;
     }
-}
+
+    //funkcija za dohvaćanje konačne cijene proizvoda
+    public static function getFinalPrice($product)
+    {
+      if($product->sale === null)
+        $final_price = $product->price;
+      else {
+        $popust = $product->price * $product->sale;
+        $final_price = $product->price - $popust;
+      }
+
+      return $final_price;
+    }
+
+    //sortiranje proizvoda uzlazno po cijeni
+    public static function sortByPriceASC($products)
+    {
+      $duljina_liste = count($products);
+
+      while($promjena !== 0)
+      {
+        $promjena = 0;
+
+        for($i = 0; i < $duljina_liste - 1; $i++)
+        {
+          $price_i = Service::getFinalPrice($products[$i]);
+          $price_iplus = Service::getFinalPrice($products[$i + 1]);
+
+          if($price_i > $price_iplus)
+          {
+            $promjena = 1;
+            $temp = $products[$i];
+            $products[$i] = $products[$i + 1];
+            $products[$i + 1] = $temp;
+          }
+        }
+      }
+    }
+
+    //sortiranje proizvoda silazno po cijeni
+    public static function sortByPriceDESC($products)
+    {
+      $duljina_liste = count($products);
+
+      while($promjena !== 0)
+      {
+        $promjena = 0;
+
+        for($i = 0; i < $duljina_liste - 1; $i++)
+        {
+          $price_i = Service::getFinalPrice($products[$i]);
+          $price_iplus = Service::getFinalPrice($products[$i + 1]);
+
+          if($price_i < $price_iplus)
+          {
+            $promjena = 1;
+            $temp = $products[$i];
+            $products[$i] = $products[$i + 1];
+            $products[$i + 1] = $temp;
+          }
+        }
+      }
+    }
+
+};
 ?>
