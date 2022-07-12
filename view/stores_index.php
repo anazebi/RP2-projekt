fa292b02ca69f10f80f5f6470a19e91abe89485f
anazebi <anazebi.math@pmf.hr>
2022-07-12 15:12:41 +0200

    pozadina

--- view/stores_index.php
+++ view/stores_index.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/_headerNav.php';
 ?>
+
+<h3>Odaberite prodavaonicu s popisa:</h3>
 <br>
 <ul class = "stores">
 <?php
@@ -10,7 +12,7 @@ require_once __DIR__ . '/_headerNav.php';
       foreach($sveTrgovine as $trgovina)
       {
         $store_name = $trgovina->name;
-        echo '<li class="stores" >';
+        echo '<li class="store" >';
         echo '<a href="index.php?rt=stores/storeInfo&imeTrgovine=' . $store_name . '">';
         echo $store_name . '<br><br>';
         echo '</a>';
